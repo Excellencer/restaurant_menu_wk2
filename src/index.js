1022521a809b9e10199f02b70d3701f067bb3415
Kalle Liljestrom <54841086+Excellencer@users.noreply.github.com>
2021-09-24 15:03:42 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -7,10 +7,48 @@ document
 
 function addComment() {
   let comment = document.getElementById("commentInput").value;
+  let rating = 1;
+  switch (parseInt(document.getElementById("rating").value, 10)) {
+    case 1:
+      rating = "Excellent";
+      break;
+    case 2:
+      rating = "Great";
+      break;
+    case 3:
+      rating = "Good";
+      break;
+    case 4:
+      rating = "Above average";
+      break;
+    default:
+      rating = "Excellent";
+  }
+
   document.getElementById("commentInput").value = "";
   let ul = document.getElementById("commentList");
   let li = document.createElement("li");
-  li.appendChild(document.createTextNode(comment));
+
+  let commentDiv = document.createElement("div");
+  commentDiv.classList.add("comment");
+
+  let commentRating = document.createElement("p");
+  commentRating.classList.add("comment-rating");
+  commentRating.appendChild(document.createTextNode(rating));
+
+  let commentText = document.createElement("p");
+  commentText.classList.add("comment-text");
+  commentText.appendChild(document.createTextNode(comment));
+
+  let commentRemove = document.createElement("button");
+  commentRemove.classList.add("remove-comment");
+  commentRemove.textContent = "Remove";
+
+  commentDiv.appendChild(commentRating);
+  commentDiv.appendChild(commentText);
+  commentDiv.appendChild(commentRemove);
+
+  li.appendChild(commentDiv);
   ul.appendChild(li);
 }
 
